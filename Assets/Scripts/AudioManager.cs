f8661f3648aa4817d9777030904a9d57ece783cd
Ishan034 <76897280+Ishan034@users.noreply.github.com>
2022-02-23 17:01:16 +0530

    Added the Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound sound in sounds)
+        {
+            sound.audioSource = gameObject.AddComponent<AudioSource>();
+            sound.audioSource.clip = sound.clip;
+
+            sound.audioSource.volume = sound.volume;
+            sound.audioSource.pitch = sound.pitch;
+            sound.audioSource.loop = sound.loop;
+        }
+    }
+
+    private void Start()
+    {
+        //would Play the soundtrack when it is added
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound sound =  Array.Find(sounds, sound => sound.name == name);
+
+        if (sound == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!"); 
+
+            return;
+        }
+
+        sound.audioSource.Play();
+    }
+}
